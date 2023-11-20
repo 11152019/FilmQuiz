a1cd623a7ab34238ec91ffa9751694a0f9d6833c
11152019 <11152019@H22239D>
2023-11-20 14:41:18 +0000

    Completed.

--- FilmQuiz/frmQuestion3.vb
+++ FilmQuiz/frmQuestion3.vb
@@ -4,8 +4,8 @@
     End Sub
 
     Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
-        If btnAnswer3.Checked Then
-            playerScore = playerscore + 1
+        If btnAnswer2.Checked Then
+            playerScore = playerScore + 1
         End If
 
         frmHighScore.Show()
